53f4c2930d770f5b8b5f329eac63f79fc427fb6e
intoccabile19 <121138353+intoccabile19@users.noreply.github.com>
2023-12-01 09:56:14 +1000

    load balancer fix

--- main.tf
+++ main.tf
@@ -47,39 +47,36 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  name = "blog-alb"
 
-  target_groups = {
-    ex-instance = {
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
-  }
+  ]
 
   http_tcp_listeners = [
     {
-      port = 80
-      protocol = "HTTP"
+      port               = 80
+      protocol           = "HTTP"
       target_group_index = 0
     }
   ]
 
   tags = {
     Environment = "dev"
-    Project     = "Example"
   }
 }
 
